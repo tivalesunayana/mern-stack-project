ca481a5e671cf8a3763b6812780da34b34ed2301
sunayanativale <sunayana@chainworks.io>
2024-04-19 19:07:23 +0530

    add Header navigation

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom"
+
+export default function Header() {
+    return(
+        <div className='bg-sky-300'>
+            <div className='flex justify-between max-w-6xl mx-auto items-center p-4'>
+            <Link to='/'>
+
+<h1>Auth App</h1>
+</Link>
+            <ul className='flex gap-4'>
+                <Link to='/'><li>Home</li></Link>
+                <Link to='/about'><li>About</li></Link>
+                <Link to='/profile'><li>Profile</li></Link>
+
+                <Link to='/sign-in'><li>SignIn</li></Link>
+
+            </ul>
+            </div>
+           
+        </div>
+    )
+}
