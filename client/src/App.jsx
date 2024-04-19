a9b9e66d0c07712560c647da580d0523549c64b2
sunayanativale <sunayana@chainworks.io>
2024-04-19 15:54:37 +0530

    add routes and pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,23 @@
-export default function App() {
-  return (
-    <h1 className="text-red-500">
-      Hello world!
-    </h1>
+import {BrowserRouter , Routes,Route} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Profile from './pages/Profile'
+
+export default function App(){
+  return(
+
+    <BrowserRouter>
+  <Routes>
+  <Route path='/' element={<Home />} />
+  <Route path='/about' element={<About/>} />
+  <Route path='/sign-in' element={<SignIn/>} />
+  <Route path='/Sign-up' element={<SignUp/>} />
+  <Route path='/profile' element={<Profile/>} />
+  <Route path='/' element={Home} />
+  </Routes>
+    </BrowserRouter>
   )
+ 
 }
